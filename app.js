0808f925d30c757c96d591568d488b44dd699752
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-14 00:42:02 +0600

    ignore API

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req,res){
+res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members:[
+            { 
+                email_address:email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME : lastName,
+
+                }
+           }
+        ]
+    };
+});
+
+    const jsonData = JSON.stringify('data');
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/List_ID";
+
+
+   const options = {
+       method: "POST",
+       auth: "fahim :API_KEY",
+   }
+
+    
+    https.request(url, options, function(response){
+        request.on("data", function(data){
+            console.log(JSON.parse(data));
+            
+        });
+
+});
+
+request.write(jsonData);
+request.end();
+
+
+
+
+
+app.listen(3000, function(req, res){
+    console.log("server is running on port: 3000");
+
+});
+
+
